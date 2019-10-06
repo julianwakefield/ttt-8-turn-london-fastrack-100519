fe7a13782e67bc0164ab0fee916a774a57bf2c18
Julian Wakefield <julianwakefield@outlook.com>
2019-10-06 13:33:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ end
 
 def turn(board)
 
-  puts "Please enter 1-9:"
+  # puts "Please enter 1-9:"
   user_input = gets.strip
   index_2 = input_to_index(user_input)
   player_move = valid_move?(board, index_2)
